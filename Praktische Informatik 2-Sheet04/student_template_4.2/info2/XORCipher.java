0e090c1cf1119f66f5e7eb41c70779d13defaa0a
Max <max@staneker.com>
2025-05-18 22:11:20 +0200

    Update XORCipher.java

--- Praktische Informatik 2-Sheet04/student_template_4.2/info2/XORCipher.java
+++ Praktische Informatik 2-Sheet04/student_template_4.2/info2/XORCipher.java
@@ -22,7 +22,7 @@ public class XORCipher {
     // Exercise 2 (a)
     // ----------------------------------------------------------------
 
-    // TODO: implement valueToChar
+
     // Take Value from CHAR_TABLE and return the corresponding character
     public static char valueToChar(int value) {
         return CHAR_TABLE[value];
@@ -33,7 +33,7 @@ public class XORCipher {
     // Exercise 2 (b) 
     // ----------------------------------------------------------------
     
-    // TODO: implement charToValue
+
     public static int charToValue(char c) {
         for (int i = 0; i < CHAR_TABLE.length; i++) {
             if (CHAR_TABLE[i] == c) {
@@ -49,10 +49,12 @@ public class XORCipher {
     // ----------------------------------------------------------------
     public static int[] stringToValues(String str) {
         if(str == null) return null;
-
-        int[] values = new int [str.length()];
+        new StringBuilder(str.length());
+        int[] values = new int[str.length()];
         for (int i = 0; i < str.length(); i++) {
-            values[i] = charToValue(str.charAt(i));
+            char c = str.charAt(i);
+            int value = charToValue(c);
+            values[i] = value;
         }
         return values;
     }
@@ -62,15 +64,15 @@ public class XORCipher {
     // Exercise 2 (d)
     // ----------------------------------------------------------------
     
-    // TODO: implement valuesToString
+   
     public static String valuesToString(int[] values) {
         if(values == null) return null;
         
-        String result = "";
-        for(int i = 0; i < values.length; i++) {
-            result += valueToChar(i);
+        StringBuilder sb = new StringBuilder(values.length);
+        for (int v : values) {          // ←  nicht den Index, sondern den Wert nehmen!
+            sb.append(valueToChar(v));
         }
-        return result;
+        return sb.toString();
     }
     
     
@@ -78,7 +80,7 @@ public class XORCipher {
     // Exercise 2 (e)
     // ----------------------------------------------------------------
 
-    // TODO: implement encryptDecrypt for type int[]
+  
     public int[] encryptDecrypt(int[] values, int key) {
         int[] result = new int[values.length];
         for(int i = 0; i < values.length; i++) {
@@ -92,10 +94,10 @@ public class XORCipher {
     // Exercise 2 (f)
     // ----------------------------------------------------------------
     
-    // TODO: implement encryptDecryot for type String
+  
     public static String encryptDecrypt(String msg, String key) {
-        int[] values = stringToValue(msg);
-        int[] keyValues = stringToValue(key);
+        int[] values = stringToValues(msg);
+        int[] keyValues = stringToValues(key);
         int[] result = new int[values.length];
         
         for (int i = 0; i < values.length; i++) {
