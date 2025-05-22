33a7c18452564b7578cf733333fd073cbbe1ab37
Max <max@staneker.com>
2025-05-22 15:29:44 +0200

    added setters

--- Praktische Informatik 2-Sheet05/student_template_5.2/info2/Point.java
+++ Praktische Informatik 2-Sheet05/student_template_5.2/info2/Point.java
@@ -3,11 +3,11 @@ package info2;
 public class Point {
     private double x, y;
 
-    Point() {
+    public Point() {
         this.x = 0;
         this.y = 0;
     }
-    Point(double x, double y) {
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
@@ -17,10 +17,19 @@ public class Point {
     public double getY() {
         return this.y;
     }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
     public double[] toArray() {
         double[] array = new double[2];
         array[0] = this.x;
         array[1] = this.y;
         return array;
     }
+
+
 }
