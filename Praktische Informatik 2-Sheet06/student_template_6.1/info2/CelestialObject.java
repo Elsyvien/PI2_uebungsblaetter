6f740cab7a1f5a664af1c43f290cdae90feae2f8
Max <max@staneker.com>
2025-05-26 19:23:26 +0200

    Update CelestialObject.java

--- Praktische Informatik 2-Sheet06/student_template_6.1/info2/CelestialObject.java
+++ Praktische Informatik 2-Sheet06/student_template_6.1/info2/CelestialObject.java
@@ -1,3 +1,11 @@
 public class CelestialObject {
+    private double mass;
+
+    public void setMass (double mass) {
+        this.mass = mass;
+    }
+    public double getMass() {
+        return mass;
+    }
 
 }
