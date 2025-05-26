88e859cca0f07edb8b5bd9c7fb5ee7421883ea74
Max <max@staneker.com>
2025-05-26 19:29:22 +0200

    fixed check

--- Praktische Informatik 2-Sheet06/student_template_6.1/info2/CelestialObject.java
+++ Praktische Informatik 2-Sheet06/student_template_6.1/info2/CelestialObject.java
@@ -1,7 +1,13 @@
 public class CelestialObject {
     private double mass;
 
+    public CelestialObject(double mass) {
+        this.mass = mass;
+    }
+
     public void setMass (double mass) {
+        if(mass < 0)
+            return;
         this.mass = mass;
     }
     public double getMass() {
