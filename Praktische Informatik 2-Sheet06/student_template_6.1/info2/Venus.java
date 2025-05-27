a813038eceda10eb9c8da10594ab7fb4002ac22a
Max <max@staneker.com>
2025-05-27 13:48:41 +0200

    Delete Venus.java

--- Praktische Informatik 2-Sheet06/student_template_6.1/info2/Venus.java
+++
@@ -1,7 +0,0 @@
-public class Venus extends Planet {
-    public Venus() {
-        super();
-        this.infectWithHumans(0); // Initialize with no infection
-        this.setMass(4.87 * Math.pow(10, 24)); // Set mass to 4.87 * 10^24 kg
-    }
-}
