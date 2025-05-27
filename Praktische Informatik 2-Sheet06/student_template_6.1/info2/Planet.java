8f2b73046be7ac859d95201d2951cbe84fca24fb
Max <max@staneker.com>
2025-05-27 13:28:40 +0200

    fixed issues in implementation

--- Praktische Informatik 2-Sheet06/student_template_6.1/info2/Planet.java
+++ Praktische Informatik 2-Sheet06/student_template_6.1/info2/Planet.java
@@ -1,18 +1,21 @@
 public class Planet extends CelestialObject {
     private long degreeOfInfection;
 
-    public Planet(double mass) {
-        super(mass);
-        this.degreeOfInfection = 0;
-    }
-
     public long getDegreeOfInfection() {
         return degreeOfInfection;
     }
-    public void setDegreeOfInfection(long degreeOfInfection) {
-        this.degreeOfInfection = degreeOfInfection;
+
+    public void infectWithHumans(long degree) {
+        if (degree < 0) {
+            this.degreeOfInfection = 0;
+            System.out.println("Infection degree cannot be negative. Resetting to 0.");
+            return;
+        }
+        this.degreeOfInfection += degree;
     }
-    public void infect() {
-        degreeOfInfection++;
+
+    public void heal() {
+        this.degreeOfInfection = 0;
+        System.out.println("Planet healed. Infection degree reset to 0.");
     }
 }
