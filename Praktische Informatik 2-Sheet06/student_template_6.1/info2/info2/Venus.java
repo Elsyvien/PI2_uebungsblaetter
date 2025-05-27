93cd6298d8cf445fb0e3c0c1c5dfb409c268de8a
Max <max@staneker.com>
2025-05-27 13:50:34 +0200

    Update Venus.java

--- Praktische Informatik 2-Sheet06/student_template_6.1/info2/info2/Venus.java
+++ Praktische Informatik 2-Sheet06/student_template_6.1/info2/info2/Venus.java
@@ -3,7 +3,5 @@ package info2;
 public class Venus extends Planet {
     public Venus() {
         super();
-        this.infectWithHumans(0); // Initialize with no infection
-        this.setMass(4.87 * Math.pow(10, 24)); // Set mass to 4.87 * 10^24 kg
     }
 }
