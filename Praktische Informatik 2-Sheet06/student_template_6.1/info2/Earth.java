855430a6fa71f22eeca75d6365ceaeedd810f762
Max <max@staneker.com>
2025-05-27 13:36:34 +0200

    added weight

--- Praktische Informatik 2-Sheet06/student_template_6.1/info2/Earth.java
+++ Praktische Informatik 2-Sheet06/student_template_6.1/info2/Earth.java
@@ -4,5 +4,6 @@ public class Earth extends Planet {
     public Earth() {
         super();
         this.infectWithHumans((long)(8 * Math.pow(10, 9))); // Initialize with no infection
+        this.setMass(6 * Math.pow(10, 24)); // Set mass to 2.5 * 10^24 kg
     }
 }
