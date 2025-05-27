c9e1e4e0e9c6a649f700231f32cf37d6016c42c2
Max <max@staneker.com>
2025-05-27 13:48:17 +0200

    moved into own package

---
+++ Praktische Informatik 2-Sheet06/student_template_6.1/info2/info2/CelestialObject.java
@@ -0,0 +1,17 @@
+package info2;
+
+public class CelestialObject {
+    private double mass;
+
+    public void setMass (double mass) {
+        if(mass < 0) {
+            System.out.println("Mass should be a positive number. Returning.");
+            return;
+        }
+        this.mass = mass;
+    }
+    public double getMass() {
+        return mass;
+    }
+
+}
---
+++ Praktische Informatik 2-Sheet06/student_template_6.1/info2/info2/Earth.java
@@ -0,0 +1,11 @@
+package info2;
+
+import java.lang.Math;
+
+public class Earth extends Planet {
+    public Earth() {
+        super();
+        this.infectWithHumans((long)(8 * Math.pow(10, 9))); // Initialize with no infection
+        this.setMass(6 * Math.pow(10, 24)); // Set mass to 2.5 * 10^24 kg
+    }
+}
