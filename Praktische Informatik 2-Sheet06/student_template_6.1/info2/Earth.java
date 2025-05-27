f222d4ce544355417279ad779b7015ec556ce4b9
Max <max@staneker.com>
2025-05-27 13:48:27 +0200

    Delete Earth.java

--- Praktische Informatik 2-Sheet06/student_template_6.1/info2/Earth.java
+++
@@ -1,9 +0,0 @@
-import java.lang.Math;
-
-public class Earth extends Planet {
-    public Earth() {
-        super();
-        this.infectWithHumans((long)(8 * Math.pow(10, 9))); // Initialize with no infection
-        this.setMass(6 * Math.pow(10, 24)); // Set mass to 2.5 * 10^24 kg
-    }
-}
