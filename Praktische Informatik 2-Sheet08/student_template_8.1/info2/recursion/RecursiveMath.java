875be45703b761ead207cd23e069c25b042c1a6c
Max <max@staneker.com>
2025-06-23 10:55:43 +0200

    Update RecursiveMath.java

--- Praktische Informatik 2-Sheet08/student_template_8.1/info2/recursion/RecursiveMath.java
+++ Praktische Informatik 2-Sheet08/student_template_8.1/info2/recursion/RecursiveMath.java
@@ -15,32 +15,70 @@ public class RecursiveMath {
     // Exercise 8.1 a
     // ----------------------------------------------------------------
     public static int add(int a, int b) {
-        // TODO: Implement me.
-        return 0;
+        if (b == 0) {
+            return a; // Base case: if b is 0, return a
+        }
+        if (b > 0) {
+            return add(incr(a), decr(b)); // Increment a and decrement b
+        } else {
+            return add(decr(a), incr(b)); // Decrement a and increment b
+        }
     }
     
     // ----------------------------------------------------------------
     // Exercise 8.1 b
     // ----------------------------------------------------------------
     public static int sub(int a, int b) {
-        // TODO: Implement me.
-        return 0;
+        if (b == 0) {
+            return a; // Base case: if b is 0, return a
+        }
+        if (b > 0) {
+            return sub(decr(a), decr(b)); // Decrement both a and b
+        } else {
+            return sub(incr(a), incr(b)); // Increment both a and b
+        }
     }
     
     // ----------------------------------------------------------------
     // Exercise 8.1 c
     // ----------------------------------------------------------------
     public static int mul(int a, int b) {
-        // TODO: Implement me.
-        return 0;
+        if( a == 0 || b == 0) {
+            return 0; // Base case: if either a or b is 0, return 0
+        }
+        if (b > 0) {
+            return add(a, mul(a, decr(b))); // Add a to the result of mul(a, b-1)
+        } else {
+            return sub(0, mul(a, decr(-b))); // Handle negative b by decrementing -b
+        }
     }
     
     // ----------------------------------------------------------------
     // Exercise 8.1 d
     // ----------------------------------------------------------------
     public static int div(int a, int b) {
-        // TODO: Implement me.
-        return 0;
+       if(a == 0 || b == 0) {
+            return 0; // Base case: if a is 0, return 0; division by zero is not handled
+        }
+        int sign = 1;
+        if (a < 0 ) {
+            a = sub(0, a); // Make a positive if it is negative
+            sign = sub(0, sign); // Change sign to negative
+        }
+        if (b < 0) {
+            b = sub(0, b); // Make b positive if it is negative
+            sign = sub(0, sign); // Change sign to negative
+        }
+        if (a < b) {
+            return 0; // If a is less than b, return 0
+        } else {
+            int result = incr(div(sub(a, b), b));
+            if (sign < 0) {
+                return sub(0, result); // If the sign is negative, return the negative result
+            } else {
+                return result; // Otherwise, return the positive result
+            }
+        }
     }
     public static void main(String[] args) {
         
